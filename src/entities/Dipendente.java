7cd3e2620ac81c56ebd50e404a0a558389eaf79a
Marissa Burca <Marissaburca@gmail.com>
2023-11-30 16:49:22 +0100

    devo imparare ad usare setter e getter

--- src/entities/Dipendente.java
+++ src/entities/Dipendente.java
@@ -30,9 +30,7 @@ public class Dipendente {
         return stipendioBase;
     }
 
-    public int getMatricola() {
-        return matricola;
-    }
+
 
     public double getStipendio() {
         return stipendio;
