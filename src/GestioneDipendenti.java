6ca0ac5e5d56b66077910a39e6348b30e89b1daf
Marissa Burca <Marissaburca@gmail.com>
2023-11-30 16:41:50 +0100

    finito stranamente

--- src/GestioneDipendenti.java
+++ src/GestioneDipendenti.java
@@ -1,10 +1,32 @@
 import entities.Dipartimento;
 import entities.Dipendente;
+import entities.Livello;
 
 public class GestioneDipendenti{
     public static void main(String[] args) {
-       Dipendente marissa = new Dipendente(1998,Dipartimento.PRODUZIONE);
-       Dipendente mario= new Dipendente(1987,Dipartimento.PRODUZIONE);
-       Dipendente luigi= new Dipendente()
+        Dipendente[] dipendenti = {
+                new Dipendente(1998, Dipartimento.PRODUZIONE),
+                new Dipendente(1987, Dipartimento.PRODUZIONE),
+                new Dipendente( 1980, Livello.IMPIEGATO, Dipartimento.AMMINISTRAZIONE,40),
+                new Dipendente(1975, Livello.DIRIGENTE, Dipartimento.VENDITE,50)
+        };
+
+        for (Dipendente dipendente : dipendenti) {
+            dipendente.promuovi();
+            dipendente.stampaDatiDipendente();
+        }
+        double totalSalaries = 0;
+
+        for (Dipendente dipendente : dipendenti) {
+            double salary = dipendente.getStipendio();
+            double importoOrarioStraordinario = dipendente.getImportoOrarioStraordinario();
+
+            // Calcola la paga con 5 ore di straordinario per ciascun dipendente
+            double salaryWithExtra = salary + (importoOrarioStraordinario * 5);
+
+            totalSalaries +=salaryWithExtra;
+        }
+
+        System.out.println("La somma degli stipendi con 5 ore di straordinario per tutti i dipendenti è: " + totalSalaries);
     }
 }
--- src/entities/Dipendente.java
+++ src/entities/Dipendente.java
@@ -1,6 +1,7 @@
 package entities;
 
 public class Dipendente {
+    //dichiaro tutti gli attributi
     private double stipendioBase= 1000;
     private int matricola;
     private double stipendio;
@@ -8,6 +9,7 @@ public class Dipendente {
     private Livello livello;
     private Dipartimento dipartimento;
 
+    //dichiaro i setter e i getter
     public void setLivello(Livello livello) {
         this.livello = livello;
     }
@@ -40,18 +42,24 @@ public class Dipendente {
         return importoOrarioStraordinario;
     }
 
+    //imposto costruttori, per avere uno stampino per ogni nuovo dipendente che creerò
     public Dipendente(int matricola,Dipartimento dipartimento ) {
-        this.matricola = matricola;
-        this.dipartimento = dipartimento;
-        this.stipendio= stipendioBase;
-        this.importoOrarioStraordinario =30;
-        this.livello= Livello.OPERAIO;
+        this.matricola = matricola; // assegno direttamente matricola
+        this.dipartimento = dipartimento; // assegno direttamente tipo dipartimento
+        this.stipendio= getStipendioBase(); //leggo il valore
+        this.importoOrarioStraordinario = 30; //fisso un valore di default per i nuovi generati
+        this.livello= Livello.OPERAIO; //set valido per tutti i nuovi dipendenti generati con costruttore
     }
-    public Dipendente(int matricola, double stipendio, Livello livello, Dipartimento dipartimento)∏{
-        this.stipendio= stipendio;
-        this.importoOrarioStraordinario =importoOrarioStraordinario;
+    public Dipendente(int matricola,Livello livello, Dipartimento dipartimento, double importoOrarioStraordinario){
+        this.stipendioBase= getStipendioBase();  //leggo il valore
+        this.stipendio=getStipendio();  //leggo il valore
+        this.matricola = matricola; // assegno direttamente matricola
+        this.livello = livello; // assegno direttamente tipo livello
+        this.dipartimento = dipartimento; // assegno direttamente tipo dipartimento
+        this.importoOrarioStraordinario = importoOrarioStraordinario; // assegno direttamente valore
     }
 
+    //metodi
     public void stampaDatiDipendente(){
         System.out.println("Matricola: " + matricola);
         System.out.println("Stipendio Base: " + stipendioBase);
@@ -59,28 +67,27 @@ public class Dipendente {
         System.out.println("Importo Orario Straordinario: " + importoOrarioStraordinario);
         System.out.println("Livello: " + livello);
         System.out.println("Dipartimento: " + dipartimento);
+
+        //stampo tutti i dati degli dipendenti
     }
     public void promuovi(){
         if(this.livello==Livello.OPERAIO){
             this.livello = Livello.IMPIEGATO;
-            this.stipendioBase= stipendioBase*1.2;
+            this.stipendio= stipendioBase*1.2;
         } else if (this.livello==Livello.IMPIEGATO) {
             this.livello = Livello.QUADRO;
-            this.stipendioBase= stipendioBase*1.5;
+            this.stipendio=stipendioBase*1.5;
         } else if (this.livello==Livello.QUADRO) {
             this.livello = Livello.DIRIGENTE;
-            this.stipendioBase= stipendioBase*2;
+            this.stipendio= stipendioBase*2;
         } else {
+            this.livello = Livello.DIRIGENTE;
+            this.stipendio= stipendioBase*2;
             System.out.println("Error during promotion");
         }
         System.out.println("Il nuovo livello del dipendente è: "+ this.livello);
-    }
-    public static double calcolaPaga(Dipendente dipendente) {
-        return dipendente.getStipendioBase();
-    }
-    public static double calcolaPaga(Dipendente dipendente, double oreStraordinario) {
-        double pagaStraordinario = oreStraordinario * dipendente.getImportoOrarioStraordinario();
-        return dipendente.getStipendioBase() + pagaStraordinario;
+
+        //promuovo i dipendenti in base al loro livello
     }
 
 }
